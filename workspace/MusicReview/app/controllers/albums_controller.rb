88413cbbccb02d2d911ad30b6e68da13ec9c28b1
Dejan Gajsek <dgajsek@gmail.com>
2015-09-05 17:30:22 +0200

    Add paperclip for image upload

--- workspace/MusicReview/app/controllers/albums_controller.rb
+++ workspace/MusicReview/app/controllers/albums_controller.rb
@@ -63,6 +63,6 @@ class AlbumsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def album_params
-      params.require(:album).permit(:artist, :album, :description, :release, :rating)
+      params.require(:album).permit(:artist, :album, :description, :release, :rating, :image)
     end
 end
